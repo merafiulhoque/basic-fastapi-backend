a1572a635e9b8b53397914ccf206761775b5eb89
merafiulhoque <me.rafiulhoque@gmail.com>
2026-02-16 21:06:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+app = FastAPI()
+
+
+
+@app.get("/")
+async def read_root(request: Request):
+    data: dict | None = await request.json()
+    print(data)
+    return JSONResponse(content=data)
+
+@app.get("/jokes")
+def get_jokes():
+    return JSONResponse(content={"joke": "Why did the scarecrow win an award? Because he was outstanding in his field!"})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
